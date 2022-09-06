80750a84b3c29cf73b09ffdf2ec360df74952815
Martin DeMello <mdemello@google.com>
2022-09-05 17:51:56 -0700

    set up toggle

--- foo/components/parent.py
+++ foo/components/parent.py
@@ -2,4 +2,7 @@ from django_unicorn.components import UnicornView
 
 
 class ParentView(UnicornView):
-    pass
+    active_view = "child1"
+
+    def toggle(self):
+        self.active_view = 'child1' if self.active_view == 'child2' else 'child2'
